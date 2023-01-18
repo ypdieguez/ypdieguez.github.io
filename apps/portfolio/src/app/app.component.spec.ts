fe32983e905645376c58e0c709b5420606ac2066
Yordan P. Dieguez <ypdieguez@gmail.com>
2023-01-17 22:21:00 -0500

    chore: move to a monorepo project witn nx + tailwindcss

--- apps/portfolio/src/app/app.component.spec.ts
+++ apps/portfolio/src/app/app.component.spec.ts
@@ -1,16 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
 
@@ -20,16 +17,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cv'`, () => {
+  it(`should have as title 'portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cv');
+    expect(app.title).toEqual('portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('cv app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Welcome portfolio'
+    );
   });
 });
